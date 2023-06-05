354a866830db50a54dabefeb3cfb317aedabb1af
aniket <aniketavinashmore343@gmail.com>
2023-06-05 07:32:55 -0700

    fix: fix notes

--- src/functions/hello-world/index.js
+++ src/functions/hello-world/index.js
@@ -2,12 +2,18 @@
 const path = require("path");
 const fs = require("fs");
 
-// example of how to load location of folders from layers depending upon local or production(lambda) runtime
-// node_modules are automatically loaded in PATH so no need to do this packages can be directly loaded
+/*
+ * when layers are loaded by lamba they are loaded in /opt directory hence dynamic require functions must be used to load the files correctly
+ * NODE_ENV environment variable is used to resolve the correct path of the common folder depending upon where the code is running on local machine or in lambda runtime
+ * this is why NODE_ENV needs to be set to production while deploying the lambda function
+ */
 const COMMON_PATH =
   process.env.NODE_ENV == "production"
     ? "/opt/nodejs/common"
     : path.join(process.cwd(), "src", "common");
+
+// example of how to get types for dynamic requires
+/**@type {import('../../common/utils')} */
 const utils = require(COMMON_PATH + "/utils.js");
 
 const { addDays } = require("date-fns");
